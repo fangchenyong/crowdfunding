bacf70bbaf6deb2a906cb82a3b38a49e9debdd59
fangchenyong <1013659102@qq.com>
2020-08-10 16:01:17 +0800

    整合SpringSecurity 内存登录

---
+++ crowdfunding-admin-parent/crowdfunding-admin-component/src/main/java/com/fangchy/mvc/config/WebAppSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.fangchy.mvc.config;
+
+import com.fangchy.constant.CrowdConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication().withUser("joey").password("123456").roles("ADMIN");
+        //builder.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+                .authorizeRequests()
+                .antMatchers("/index.html")
+                .permitAll()
+                .antMatchers("/bootstrap/**")
+                .permitAll()
+                .antMatchers("/css/**")
+                .permitAll()
+                .antMatchers("/fonts/**")
+                .permitAll()
+                .antMatchers("/img/**")
+                .permitAll()
+                .antMatchers("/jquery/**")
+                .permitAll()
+                .antMatchers("/layer/**")
+                .permitAll()
+                .antMatchers("/script/**")
+                .permitAll()
+                .antMatchers("/ztree/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable()
+                .formLogin()
+                .loginPage("/admin/to/login/page.html")
+                .permitAll()
+                .loginProcessingUrl("/security/do/login.html")
+                .permitAll()
+                .usernameParameter("loginAcct")
+                .passwordParameter("userPswd")
+                .defaultSuccessUrl("/admin/to/main/page.html")
+                .and()
+                .logout()
+                .logoutUrl("/security/do/logout.html")
+                .logoutSuccessUrl("/admin/to/login/page.html");
+    }
+}
--- crowdfunding-admin-parent/crowdfunding-admin-webui/src/main/webapp/WEB-INF/include-nav.jsp
+++ crowdfunding-admin-parent/crowdfunding-admin-webui/src/main/webapp/WEB-INF/include-nav.jsp
@@ -23,7 +23,7 @@
 							<li><a href="#"><i class="glyphicon glyphicon-comment"></i>
 									消息</a></li>
 							<li class="divider"></li>
-							<li><a href="admin/do/logout.html"><i
+							<li><a href="security/do/logout.html"><i
 									class="glyphicon glyphicon-off"></i> 退出系统</a></li>
 						</ul>
 					</div>
